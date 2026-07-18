0a37a735b6e436e1b22f9ea831834e6bf66d1e7b
Furkan Dereli <frkndereli@gmail.com>
2026-07-18 23:07:46 +0300

    init

---
+++ melasoft_task/src/test/java/com/dereli/melasoft_task/MelasoftTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dereli.melasoft_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelasoftTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
